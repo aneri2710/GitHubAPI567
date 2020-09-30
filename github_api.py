8aec726307b55abd6d47ac3696f7ba260b7f9e42
aneri2710 <anerishah.2710@gmail.com>
2020-09-30 14:57:32 -0400

    HW04

---
+++ github_api.py
@@ -0,0 +1,44 @@
+"""
+Author : Aneri Shah
+CWID : 10451737
+
+Homework 4- To get the repository name, number of commits from the user ID from github.
+
+"""
+import requests
+import json
+
+
+def github_user_info(userID):
+    # check if the user id is invalid
+    if userID is None:
+        return "Input is invalid"
+    # check if the user is is not a string
+    if not isinstance(userID, str):
+        raise ValueError(f"{userID} is not a string.")
+
+    user_repo = "https://api.github.com/users/" + userID + "/repos"
+
+    get_repos = requests.get(user_repo)
+    repos_json = get_repos.json()
+
+    for value in repos_json:
+        repository_name = value.get("name")
+        commits = "https://api.github.com/repos/" + userID + "/" + repository_name + "/commits"
+        get_commits = requests.get(commits)
+        commits_json = get_commits.json()
+        count = 0
+        # check if the number of commits are 0
+        if commits_json == 0:
+            print("no commits")
+
+        for c in commits_json:
+            count += 1
+        yield f"Repo: {repository_name}, Number of commits: {count}"
+
+
+def main():
+    # get the user id
+    userID = input("Enter your user ID: ")
+    for i in github_user_info(userID):
+        return i
---
+++ github_api_test.py
@@ -0,0 +1,29 @@
+"""
+
+Test case for the github get repository name and number of commits.
+
+"""
+
+import unittest
+from GitHubAPI_567.github_api import github_user_info
+
+
+class TestGitHubAPI(unittest.TestCase):
+
+    def test_githubapi(self):
+
+        repository = [value for value in github_user_info("richkempinski")]
+        result = ['Repo: csp, Number of commits: 2',
+                  'Repo: hellogitworld, Number of commits: 30',
+                  'Repo: helloworld, Number of commits: 6',
+                  'Repo: Mocks, Number of commits: 10',
+                  'Repo: Project1, Number of commits: 2',
+                  'Repo: richkempinski.github.io, Number of commits: 9',
+                  'Repo: threads-of-life, Number of commits: 1',
+                  'Repo: try_nbdev, Number of commits: 2',
+                  'Repo: try_nbdev2, Number of commits: 5']
+        self.assertEqual(repository, result)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
