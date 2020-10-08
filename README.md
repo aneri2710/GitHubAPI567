e6854a81ef7b4bfcc068dc1734a72f16a571c2d2
aneri2710 <anerishah.2710@gmail.com>
2020-10-07 21:48:49 -0400

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![build status of master](https://travis-ci.org/aneri2710/GitHubAPI_567.svg?branch=master)](https://travis-ci.org/aneri2710/GitHubAPI_567)
 
 # GITHUBAPI
-HW_04
+HW05a_Mocking
--- github_api_test.py
+++ github_api_test.py
@@ -3,36 +3,38 @@
 Test case for the github get repository name and number of commits.
 
 """
-
+import os
 import unittest
+from mock import patch
 from github_api import github_user_info
+import json
 
 
 class TestGitHubAPI(unittest.TestCase):
 
-    def test_error(self):
-        with self.assertRaises(ValueError):
-            github_user_info(" ")
-
-    def test_type_check(self):
-        with self.assertRaises(ValueError):
-            github_user_info(1)
-
-
     def test_wrong_user_id(self):
-        with self.assertRaises(ValueError):
-            github_user_info("darshita_kreya_aneri")
+
+        mock_patcher = patch('requests.get')
+        mock_get = mock_patcher.start()
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{dir_path}/file_not_found.json') as f:
+            mock_get.return_value = json.load(f)
+        mock_patcher.stop()
+        get_fun = mock_get()
+        self.assertEqual(get_fun.get('message'), "Not Found")
 
     def test_githubapi(self):
-        self.assertEqual(github_user_info("richkempinski"), ["Repo: csp Number of commits: 2",
-                                                             "Repo: hellogitworld Number of commits: 30",
-                                                             "Repo: helloworld Number of commits: 6",
-                                                             "Repo: Mocks Number of commits: 10",
-                                                             "Repo: Project1 Number of commits: 2",
-                                                             "Repo: richkempinski.github.io Number of commits: 9",
-                                                             "Repo: threads-of-life Number of commits: 1",
-                                                             "Repo: try_nbdev Number of commits: 2",
-                                                             "Repo: try_nbdev2 Number of commits: 5"])
+        mock_patcher = patch('requests.get')
+        mock_get = mock_patcher.start()
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{dir_path}/rich.json') as f:
+            mock_get.return_value = json.load(f)
+        mock_patcher.stop()
+        get_fun = mock_get()
+        self.assertEqual(len(get_fun), 73)
+
 
 
 if __name__ == '__main__':
